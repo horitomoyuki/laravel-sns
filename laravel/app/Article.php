6c02b9ee5b70ab5b07be5acadea415e2339e4d75
horitomoyuki <eziltom0131@gmail.com>
2021-10-03 21:45:45 +0900

    create-Article_post

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -7,7 +7,13 @@ use illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
-    public function user():BelongsTo {
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+    public function user():BelongsTo
+    {
         return $this->belongsTo('App\User');
     }
 }
